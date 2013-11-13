591ece6e394e1c4235c8f73bb194e8bcc4190375
Rahul Ghose <rahul.gh@directi.com>
2013-11-13 15:01:54 +0530

    removed unused files

--- varnishinterface/recv-data.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo 'Data recieved :)';
-	parse_str( $_SERVER['QUERY_STRING'], $all_data );
-	foreach(array_keys($all_data) as $key){
- 	   print $key . ' is ' .  $all_data[$key] . "\n";
-	}
-?>
